4bb98d554cedab94d83296e23e43f7fc3431e276
Kellypops <123489367+Kellypops@users.noreply.github.com>
2025-02-11 16:35:53 +0100

    - Added support for film structure to HDF5 tab

--- modules/callbacks/callbacks_hdf5.py
+++ modules/callbacks/callbacks_hdf5.py
@@ -10,3 +10,39 @@ def callbacks_hdf5(app):
 
     def test_function(n_clicks):
         return str(n_clicks)
+
+
+    # Callback to add new layers to the film
+    @app.callback(
+        [Output('hdf5_layer_dropdown', 'value', allow_duplicate=True),
+         Output('hdf5_layer_dropdown', 'options', allow_duplicate=True)],
+        Input('hdf5_layer_dropdown', 'value'),
+        State('hdf5_layer_dropdown', 'options'),
+        prevent_initial_call=True
+    )
+    def update_layer_dropdown(dropdown_value, dropdown_options):
+        if dropdown_value == 'Add layer':
+            dropdown_options.append('New layer')
+            return 'New layer', dropdown_options
+        else:
+            raise PreventUpdate
+
+
+    # Callback to set layer attributes
+    @app.callback(
+        [Output('hdf5_layer_dropdown', 'options', allow_duplicate=True),
+        Output('hdf5_layer_dropdown', 'value', allow_duplicate=True)],
+        Input('hdf5_layer_element', 'value'),
+        Input('hdf5_layer_thickness', 'value'),
+        State('hdf5_layer_dropdown', 'value'),
+        State('hdf5_layer_dropdown', 'options'),
+        prevent_initial_call=True
+    )
+    def update_layer_attributes(layer_element, layer_thickness, dropdown_value, dropdown_options):
+        if dropdown_value != 'Add layer':
+            layer_details = f'{layer_element} ({layer_thickness} nm)'
+            dropdown_options.remove(dropdown_value)
+            dropdown_options.append(layer_details)
+            return dropdown_options, layer_details
+        else:
+            raise PreventUpdate
--- modules/interface/widgets_hdf5.py
+++ modules/interface/widgets_hdf5.py
@@ -47,6 +47,71 @@ class WidgetsHDF5:
             ],
         )
 
+        # Widget for the right box (sample info)
+        self.hdf5_right = html.Div(
+            className="subgrid top-right",
+            children=[
+                html.Div(
+                    className="subgrid-1",
+                    children=[
+                        html.Label("Fabrication date"),
+                        dcc.Input(
+                            className="long-item",
+                            id='hdf5_fabrication_date',
+                            type='text',
+                            placeholder='Fabrication date'
+                        ),
+                        html.Label("Sample Name"),
+                        dcc.Input(
+                            className="long-item",
+                            id='hdf5_sample_name',
+                            type='text',
+                            placeholder='Sample name'
+                        ),
+                        html.Label("Operator Name"),
+                        dcc.Input(
+                            className="long-item",
+                            id='hdf5_operator_name',
+                            type='text',
+                            placeholder='Operator name'
+                        )
+                    ]
+                ),
+                html.Div(
+                    className="subgrid-3",
+                    children=[
+                            dcc.Dropdown(
+                                className='long-item',
+                                id="hdf5_layer_dropdown",
+                                options=['Add layer']
+                            ),
+                        html.Div(children=[
+                            dcc.Input(
+                                id="hdf5_layer_element",
+                                type='text',
+                                placeholder='Layer element',
+                                debounce=True
+                            ),
+                            dcc.Input(
+                                id="hdf5_layer_thickness",
+                                type='number',
+                                placeholder='Layer thickness',
+                                debounce=True
+                            )],
+                        style={'display': 'flex', 'gap': '10px'}
+                        )
+                    ]
+                )
+            ]
+        )
+
+        # Stored variables
+        self.hdf5_stores = html.Div(
+            children=[
+                dcc.Store(id="hdf5_layer_structure_store", data=None),
+            ]
+        )
+
 
     def make_tab_from_widgets(self):
         hdf5_tab = dcc.Tab(
@@ -58,6 +123,7 @@ class WidgetsHDF5:
                     [
                         self.hdf5_left,
                         self.hdf5_center,
+                        self.hdf5_right,
                     ],
                     className="grid-container",
                 )
